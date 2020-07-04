e333dd8decf843bfcf1c8131973be15c947f5253
NicholasFarrow <nicholas.w.farrow@gmail.com>
2020-07-05 03:18:06 +1000

    suport multibranches

--- ipupdate.py
+++ ipupdate.py
@@ -29,27 +29,45 @@ def update_aliasrc(lines, newip):
     
     return
 
+def git_stash(newbranch):
+    prior_branch = subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"], cwd=gitloc).decode('utf-8').strip()
+    print(prior_branch)
 
-def update_git():
-    gc = subprocess.check_output(["git", "checkout", branch], cwd=gitloc)
+    gs = subprocess.check_output(["git", "stash"], cwd=gitloc)
+    print(gs.decode('unicode_escape'))
+    
+    gc = subprocess.check_output(["git", "checkout", newbranch], cwd=gitloc)
     print(gc.decode('unicode_escape'))
 
+    return prior_branch
+
+
+def git_restore(prior_branch):
+    gc = subprocess.check_output(["git", "checkout", prior_branch], cwd=gitloc)
+    print(gc.decode('unicode_escape'))
+
+    gs = subprocess.check_output(["git", "stash", "pop"], cwd=gitloc)
+    print(gs.decode('unicode_escape'))
+    return
+
+
+def update_git():
     ga = subprocess.check_output(["git", "add", aliasfile], cwd=gitloc)
     print(ga.decode('unicode_escape'))
-   
-    time.sleep(5)
-
+  
     gc = subprocess.check_output(["git", "commit", "-m", "'Automatic IP address update for rasperry pi'"], cwd=gitloc)
     print(gc.decode('unicode_escape'))
 
     gp = subprocess.check_output(["git", "push", 'origin', branch], cwd=gitloc)
     print(gp.decode('unicode_escape'))
-
+    
     return
 
-
+prior_branch = git_stash(branch)
 lines, current_ip = get_plugfiles_ip()
 print("aliasrc pi IP: {}".format(current_ip))
+git_restore(prior_branch)
+
 
 while True:
     try:
@@ -63,19 +81,23 @@ while True:
     except Exception as e:
         print(e)
         continue
-   
+  
     if ip != current_ip:
         print("Different IPs {}, {}".format(current_ip, ip))
         
+        print("Stashing any existing git changes...")
+        prior_branch = git_stash(branch)
+
         print("\n\nUpdating {}".format(aliasfile))
         update_aliasrc(lines, ip)
         
         print("\n\nUpdating git branch {}".format(branch))
         update_git()
         
+        print("\n\n Restoring stashed changes...")
+        git_restore(prior_branch)
+
         current_ip = ip
         print("Updated.")
 
     time.sleep(60*15)
-
-
